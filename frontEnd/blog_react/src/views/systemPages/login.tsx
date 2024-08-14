a2214a19b3e1bc5ad7d789535bc9d9bf6c74ea2e
Keru <68145432+wdnmd-afk@users.noreply.github.com>
2024-08-14 22:57:16 +0800

    feat(frontEnd):添加antd组件库，添加登录页

--- frontEnd/blog_react/src/views/systemPages/login.tsx
+++ frontEnd/blog_react/src/views/systemPages/login.tsx
@@ -1,13 +1,71 @@
 import React from "react";
-import { Http } from "@/utils";
+// import { Http } from "@/utils";
+import style from "@/styles/login.module.scss";
+import { Button, Form, Input, Checkbox } from "antd";
 
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 const Login: React.FC = () => {
-  const testApi = async () => {
-    const { data } = await Http.post("/user/create", {});
-    console.log(data);
-  };
+  const onFinish = () => {};
+  const onFinishFailed = () => {};
 
-  return <div onClick={testApi}>Login</div>;
+  return (
+    <div className={style.login_container}>
+      <div className={style.login_top}></div>
+      <div className={style.login_box}>
+        <div w600>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600, width: 600, color: "white" }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password size="large" />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button size="large" type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+      <div className={style.login_bottom}></div>
+    </div>
+  );
 };
 
 export default Login;
